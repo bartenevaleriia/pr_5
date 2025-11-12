ca007857eead7189295404692e47e0d14136a00f
bartenevaleriia <bartenevalera9@gmail.com>
2025-11-12 22:21:53 +0300

    пр5_2.py

--- пр5_2.py
+++ пр5_2.py
@@ -1,3 +1,13 @@
+def calculate_imt():
+    """
+    Вычисляет ИМТ по заданному росту и весу.
+    Args:
+        weight(float): Вес человека.
+        height(float): Рост человека.
+    Returns:
+         float: ИМТ по заданным параметрам.
+    """
 weight, height = map(float,input('Введите вес (в кг) и рост (в м) через пробел: ').split())
 imt = weight/(height**2)
 print(f'ИМТ = {imt:.1f}')
+
